6ffa02c3e0b0519b39d2db078ef8f89c33b00b16
michaelteshome1626 <michaelteshome1626@gmail.com>
2024-03-22 14:32:48 -0400

    created spring boot project

---
+++ vinylstore/src/main/java/com/example/vinylstore/VinylstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.vinylstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylstoreApplication.class, args);
+	}
+
+}
---
+++ vinylstore/src/test/java/com/example/vinylstore/VinylstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinylstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
